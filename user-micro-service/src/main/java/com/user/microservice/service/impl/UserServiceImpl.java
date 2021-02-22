c487965f6ad75feae367544a454212571528927c
admin <manikarthi25@gmail.com>
2021-02-22 15:18:44 +0530

    Circuit Breaker Implementation

--- user-micro-service/src/main/java/com/user/microservice/service/impl/UserServiceImpl.java
+++ user-micro-service/src/main/java/com/user/microservice/service/impl/UserServiceImpl.java
@@ -6,15 +6,27 @@ import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
+import com.user.microservice.dto.Order;
 import com.user.microservice.dto.User;
+import com.user.microservice.dto.UserOrder;
 import com.user.microservice.entity.UserEO;
+import com.user.microservice.feignclient.OrderClient;
 import com.user.microservice.repo.UserRepo;
 import com.user.microservice.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private CircuitBreakerFactory circuitBreakerFactory;
+
+	@Autowired
+	private OrderClient orderClient;
 
 	@Autowired
 	private UserRepo userRepo;
@@ -55,6 +67,35 @@ public class UserServiceImpl implements UserService {
 			return mapToDTO(userRepo.saveAndFlush(userEO));
 		}
 		return null;
+	}
+
+	@Override
+	public UserOrder getOrderByUserId(Long userId) {
+		UserOrder userOrder = new UserOrder();
+		User user = getUserByUserId(userId);
+
+		CircuitBreaker circuitBreaker = circuitBreakerFactory.create("getorder");
+
+		Order order = circuitBreaker.run(() -> orderClient.getOrderByUserId(userId).getBody(),
+				Throwable -> getDefaultMessage(userId));
+		if (ObjectUtils.isEmpty(order)) {
+			userOrder.setMessage("Please try after some time");
+		} else {
+			userOrder.setMessage("Order service uo and running, Got response from order service");
+			userOrder.setOrder(order);
+			userOrder.setUser(user);
+		}
+		return userOrder;
+	}
+
+	public Order getDefaultMessage(Long userId) {
+		return null;
+	}
+
+	@Override
+	public Order updateOrderFromUser(Order order) {
+		Order orderRes = orderClient.updateOrderByOrderId(order).getBody();
+		return orderRes;
 	}
 
 	@Override
