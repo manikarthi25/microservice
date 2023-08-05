815557b9bf5fec3ecc05bdce2c6d05537472bfbb
manikarthi25 <manikarthi25@gmail.com>
2023-08-05 22:29:40 +0530

    rework

--- bank-usecase/src/main/java/com/bank/mani/repo/TranactionHistoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bank.mani.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bank.mani.entity.TransactionHistoryEO;
-
-public interface TranactionHistoryRepo extends JpaRepository<TransactionHistoryEO, Long> {
-
-}
